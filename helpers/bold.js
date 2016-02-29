2aefcf34c62f9f710d2c3f8f8ab05cb010c17b25
zinkoff <tosti@mikbook.local>
2016-02-29 11:15:48 +0100

    renamed var

--- helpers/bold.js
+++ helpers/bold.js
@@ -1,6 +1,5 @@
-// Example file src/helpers/bold.js
 module.exports = function(options) {
   // options.fn(this) = Handelbars content between {{#bold}} HERE {{/bold}}
-  var bolder = '<strong>' + options.fn(this) + '</strong>';
-  return bolder;
+  var bold = '<strong>' + options.fn(this) + '</strong>';
+  return bold;
 }
